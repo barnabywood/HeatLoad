87a066743c6f2c4668b81962d6f3cad11de70308
barnabywood <barnaby.wood@yahoo.com>
2026-02-11 17:35:36 +0000

    Initial Commit

---
+++ HeatLoad/HeatLoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeatLoadApp.swift
+//  HeatLoad
+//
+//  Created by Barnaby Wood on 11/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct HeatLoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
