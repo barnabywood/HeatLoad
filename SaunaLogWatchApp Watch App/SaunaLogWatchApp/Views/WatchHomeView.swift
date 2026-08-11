e067138a8bb2e1bb827049a15ec162b5457442f9
barnabywood <barnaby.wood@yahoo.com>
2026-08-11 19:50:44 +0100

    Base session guidance on heat history

--- SaunaLogWatchApp Watch App/SaunaLogWatchApp/Views/WatchHomeView.swift
+++ SaunaLogWatchApp Watch App/SaunaLogWatchApp/Views/WatchHomeView.swift
@@ -372,17 +372,15 @@ struct WatchHomeView: View {
         }
 
         let typicalHeartRate = recentTypicalHeartRate
-        let currentSessionChange = health.currentSessionHeartRateChange ?? 0
-
-        if currentSessionChange >= 12 || (typicalHeartRate.map { bpm >= $0 * 1.12 } ?? false) {
+        if typicalHeartRate.map { bpm >= -e * 1.12 } ?? false {
             return .aboveUsual
         }
 
-        if currentSessionChange <= -10 || (typicalHeartRate.map { bpm <= $0 * 0.90 } ?? false) {
+        if typicalHeartRate.map { bpm <= -e * 0.90 } ?? false {
             return .belowUsual
         }
 
-        if currentSessionChange >= 6 || (typicalHeartRate.map { bpm >= $0 * 1.05 } ?? false) {
+        if typicalHeartRate.map { bpm >= -e * 1.05 } ?? false {
             return .building
         }
 
